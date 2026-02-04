3c6f352d4ae3752fa692552974f5f7e51c2790ae
zbochenek <zbochenek1@st.swps.edu.pl>
2026-02-04 20:04:51 +0100

    fix: settings, urls, materials/serializers

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 from materials.views import MaterialViewSet
 from listings.views import ListingViewSet
 from exchanges.views import ExchangeRequestViewSet
 from reviews.views import ReviewViewSet
-from users.views import UserViewSet
-from rest_framework.authtoken.views import obtain_auth_token
-from users.views import RegisterView
+from users.views import UserViewSet, RegisterView
+
 
 router = DefaultRouter()
 router.register(r'materials', MaterialViewSet)
@@ -17,10 +17,15 @@ router.register(r'exchanges', ExchangeRequestViewSet)
 router.register(r'reviews', ReviewViewSet)
 router.register(r'users', UserViewSet)
 
-#
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API (chronione tokenem)
     path('api/', include(router.urls)),
-    path('api/auth/token/', obtain_auth_token), 
-    path('api/auth/register/', RegisterView.as_view()), 
+
+    # AUTH (publiczne)
+    path('api/auth/register/', RegisterView.as_view()),
+    path('api/auth/token/', obtain_auth_token),
 ]
+

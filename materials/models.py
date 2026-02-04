25fd698d6ad58ead5c7e26c670345ca7dcd1ea01
nzaboklicka-max <nzaboklicka@st.swps.edu.pl>
2026-02-04 15:45:58 +0100

    model materials

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Material(models.Model):
+    TYPE_CHOICES = [
+        ('book', 'Książka'),
+        ('notes', 'Notatki'),
+    ]
+
+    SUBJECT_CHOICES = [
+        ('social', 'Psychologia społeczna'),
+        ('cognitive', 'Psychologia poznawcza'),
+        ('general', 'Psychologia ogólna'),
+        ('developmental', 'Psychologia rozwojowa'),
+        ('neuro', 'Neuropsychologia'),
+    ]

5928301721c74807ac05745eca23f118169ad011
zbochenek <zbochenek1@st.swps.edu.pl>
2026-02-05 08:59:28 +0100

    feat: add permissions and custom endpoints

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Listing
 from .serializers import ListingSerializer
@@ -13,3 +15,13 @@ class ListingViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=False, methods=['get'])
+    def my(self, request):
+        queryset = self.queryset.filter(
+            owner=request.user,
+            status='active'
+        )
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
--- materials/views.py
+++ materials/views.py
@@ -1,8 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Material
 from .serializers import MaterialSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class MaterialViewSet(ModelViewSet):
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
+    
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        prefix = request.query_params.get('q', '')
+        queryset = self.queryset.filter(title__istartswith=prefix)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
